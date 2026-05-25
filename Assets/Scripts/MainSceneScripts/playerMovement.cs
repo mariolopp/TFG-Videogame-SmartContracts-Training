663add01075d34a4724eb88b0c77e835d47ccc8b
Mario <125618626+mariolopp@users.noreply.github.com>
2026-05-25 22:25:35 +0200

    Added front animation when walking

--- Assets/Scripts/MainSceneScripts/playerMovement.cs
+++ Assets/Scripts/MainSceneScripts/playerMovement.cs
@@ -5,16 +5,29 @@ public class playerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // Actualizar animaciones
+        // 'movement.y' vale -1 al ir hacia abajo (Frente)
+        if (movement.y < -0.1f)
+        {
+            animator.SetBool("AndandoFrente", true);
+        }
+        else
+        {
+            animator.SetBool("AndandoFrente", false);
+        }
     }
 
     void FixedUpdate()
